71ab7d270ee83fd5c95a861f8e740d182d67c3eb
HarshalT18 <harshalthaware9049@gmail.com>
2024-12-18 16:21:28 +0530

    Done with User

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -116,16 +116,12 @@ export const updateProfile = async (req, res) => {
   try {
     const { fullname, email, phoneNumber, bio, skills } = req.body;
     const file = req.file;
-    if (!fullname || !email || !phoneNumber || !bio || !skills) {
-      return res.status(400).json({
-        message: "Something is missing",
-        success: false,
-      });
-    }
 
     // cloudinary will comes here
-
-    const skillsArray = skills.spilt(",");
+    let skillsArray;
+    if (skills) {
+      skillsArray = skills.spilt(",");
+    }
     const userId = req.id; // middleware authentication
     let user = await User.findById(userId);
     if (!user) {
@@ -135,11 +131,11 @@ export const updateProfile = async (req, res) => {
       });
     }
     //updating data
-    (user.fullname = fullname),
-      (user.email = email),
-      (user.phoneNumber = phoneNumber),
-      (user.profile.bio = bio),
-      (user.profile.skills = skillsArray);
+    if (fullname) user.fullname = fullname;
+    if (email) user.email = email;
+    if (phoneNumber) user.phoneNumber = phoneNumber;
+    if (bio) user.profile.bio = bio;
+    if (skills) user.profile.skills = skillsArray;
 
     // resume comes later
 

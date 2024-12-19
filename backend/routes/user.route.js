c7bede8f56fd4679b37097df29e1d1084a17db54
HarshalT18 <harshalthaware9049@gmail.com>
2024-12-19 12:46:24 +0530

    Updated user route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
+import {
+  login,
+  logout,
+  register,
+  updateProfile,
+} from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
